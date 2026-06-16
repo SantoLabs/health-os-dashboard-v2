62b5700e8b983e9452317140cd1a348fcc102cda
SantoLabs <ai.santolabs@gmail.com>
2026-06-16 18:30:37 +0530

    sync: app/more/page.tsx

---
+++ app/more/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { Screen } from "../components/Screen";
+export default function MorePage() {
+  return (
+    <Screen title="More">
+      <div className="card nudge">≡ Goals, medical, and settings coming here.</div>
+    </Screen>
+  );
+}
